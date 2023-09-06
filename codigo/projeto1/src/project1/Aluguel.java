45698e89c2d1fcf75b84de73dcf24927501d5ae1
Paulo Rubinger <paulopimentarubinger@gmail.com>
2023-09-05 22:57:00 -0300

    Adicionando os getters and setters

--- codigo/projeto1/src/project1/Aluguel.java
+++ codigo/projeto1/src/project1/Aluguel.java
@@ -2,4 +2,73 @@ package project1;
 
 public class Aluguel {
     
+    private static int proximoId = 1;
+    private int idAluguel = 0;
+    private Cliente cliente;
+    private Equipamento equipamento;
+    private String dataInicioDoAluguel;
+    private String dataTerminoDoAluguel;
+    private float valorTotal;
+
+    public Aluguel(Cliente cliente, Equipamento equipamento, String dataInicioDoAluguel, String dataTerminoDoAluguel, float valorTotal) {
+        this.idAluguel = proximoId;
+        this.cliente = cliente;
+        this.equipamento = equipamento;
+        this.dataInicioDoAluguel = dataInicioDoAluguel;
+        this.dataTerminoDoAluguel = dataTerminoDoAluguel;
+        this.valorTotal = valorTotal;
+        proximoId++;
+    }
+
+    public int getIdAluguel() {
+        return idAluguel;
+    }
+
+    public void setIdALuguel(int idAluguel) {
+        this.idAluguel = idAluguel;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Equipamento getEquipamento() {
+        return equipamento;
+    }
+
+    public void setEquipamento(Equipamento equipamento) {
+        this.equipamento = equipamento;
+    }
+
+    public String getDataInicioDoAluguel() {
+        return dataInicioDoAluguel;
+    }
+
+    public void setDataInicioDoAluguel(String dataInicioDoAluguel) {
+        this.dataInicioDoAluguel = dataInicioDoAluguel;
+    }
+
+    public String getDataTerminoDoAluguel() {
+        return dataTerminoDoAluguel;
+    }
+
+    public void setDataTerminoDoAluguel(String dataTerminoDoAluguel) {
+        this.dataTerminoDoAluguel = dataTerminoDoAluguel;
+    }
+
+    public float getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(float valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    // public int calcularValorDoAluguel() {
+
+    // }
 }
--- codigo/projeto1/src/project1/Equipamento.java
+++ codigo/projeto1/src/project1/Equipamento.java
@@ -21,20 +21,40 @@ public class Equipamento {
     public int getCodigo() {
         return codigo;
     }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
     
     public String getTipo() {
         return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 
     public String getDescricao() {
         return descricao;
     }
 
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
     public float getValorDiaria() {
         return valorDiaria;
     }
 
+    public void setValorDiaria(float valorDiaria) {
+        this.valorDiaria = valorDiaria;
+    }
+
     public int getQuantidade() {
         return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
     }
 }
